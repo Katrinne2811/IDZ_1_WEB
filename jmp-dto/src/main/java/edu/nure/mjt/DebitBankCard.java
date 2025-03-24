c3b7c301c55ebf48ffb1efe78d29a1477b0c8465
Katrinne2811 <160325742+Katrinne2811@users.noreply.github.com>
2025-03-24 13:39:30 +0100

    Update DebitBankCard.java

--- jmp-dto/src/main/java/edu/nure/mjt/DebitBankCard.java
+++ jmp-dto/src/main/java/edu/nure/mjt/DebitBankCard.java
@@ -1,26 +1,23 @@
 package edu.nure.mjt;
 
+//Оголошення класу для створення дебетової банківської картки
 public class DebitBankCard extends BankCard {
 
+    private double balance; // Баланс на картці
 
-    private double balance; // Приватна змінна - число з крапкою - баланс на картці
-
-    //Конструктор - формує об'єкт класу - дебетову банківську картку
+    //Конструктор, що формує об'єкт класу - дебетову банківську картку
     public DebitBankCard(String number, User user, double balance) {
         super(number, user, BankCardType.DEBIT); // Викликаємо конструктор батьківського класу BankCard, від якого
-        //дочірній клас DebitBankCard успадковує номер та користувача.
-        //До батьківського класу BankCard передається тип картки Debit
-        this.balance = balance; // додаємо нову змінну, якої нема в батьківської картки:
-        // "this.balance" — змінній балансу дебетової картки в класі DebitBankCard присвоюється значення balance з дужок конструктора
+        //дочірній клас DebitBankCard успадковує номер та користувача + до батьківського класу BankCard передається тип картки Debit
+        this.balance = balance; // додаємо змінну типу картки, якої нема в батьківському класі
         }
 
-    //Геттер - метод, який дозволяє дізнатися баланс дебетової картки
+    //Метод, який дозволяє дізнатися баланс дебетової картки
     public double getBalance() {
             return balance;
         }
 
-    //Сеттер - метод, що дозволяє змінити значення балансу картки класу DebitBankCard
-    // на значення balance з дужок
+    //Метод, що дозволяє змінити значення балансу картки 
     public void setBalance(double balance) {
             this.balance = balance;
         }
