de618990072150190a09211a4f4c36e48822f4fd
Katrinne2811 <160325742+Katrinne2811@users.noreply.github.com>
2025-03-24 13:50:16 +0100

    Update CloudService.java

--- jmp-cloud-service-impl/src/main/java/edu/nure/mjt/cloud/serv/CloudService.java
+++ jmp-cloud-service-impl/src/main/java/edu/nure/mjt/cloud/serv/CloudService.java
@@ -5,29 +5,29 @@ import edu.nure.mjt.BankCard;
 import edu.nure.mjt.Subscription;
 import edu.nure.mjt.User;
 import java.time.LocalDateTime;
-import java.util.HashMap;//Клас, об'єкти якого зберігають пари "значення-ключ"
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;//Клас, об'єкти якого зберігають певні значення
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;//Клас, що визначає структури даних, у які збираються дані з потоку
 
 //Створення класу, що реалізує інтерфейс Service
 public class CloudService implements Service {
-    //Незмінне сховище даних зі збереженими про банківські картки записами за ключем - номером картки
+    //Сховище даних із записами про банківські картки з ключем - номером картки
     private final Map<String, Subscription> subscriptions = new HashMap<>();
-    //Незмінне сховище даних зі збереженими банківськими картками з ключем - номером картки
+    //Сховище даних зі збереженими створеними банківськими картками з ключем - номером картки
     private final Map<String, BankCard> cards = new HashMap<>();
 
 
-    //Пропишемо тіло методу subscribe з інтерфейсу Service
+    //Тіло методу для створення запису про картку з інтерфейсу Service
     @Override
     public void subscribe(BankCard bankCard) {
-        //Змінна номеру банк. картки
+        //Номер банк. картки
         String cardNumber = bankCard.getNumber();
-        //Булева змінна - повертає істинність наявності запису про картку з вказаним номером
+        //Істинність наявності запису про картку з вказаним номером
         var alreadySubscribed = subscriptions.keySet().stream().anyMatch(number -> number.equals(cardNumber));
 
-        //Якщо запису ще нема, то він створюється та зберігається в БД записів та карток
+        //Якщо запису ще нема, то він створюється та зберігається в сховищах записів та створених банк. карток
         if (!alreadySubscribed) {
             var subscription = new Subscription(cardNumber, LocalDateTime.now());
             subscriptions.put(cardNumber, subscription);
@@ -35,21 +35,20 @@ public class CloudService implements Service {
         }
     }
 
-    //Пропишемо тіло методу отримання запису getSubscriptionByBankCardNumber за ключем з інтерфейсу Service
+    //Тіло методу отримання запису за ключем з інтерфейсу Service
     @Override
     public Optional<Subscription> getSubscriptionByBankCardNumber(String bankCardNumber) {
         //Отримання запису про картку в банку за її номером
         Subscription subscription = subscriptions.get(bankCardNumber);
-        //Якщо запис існує, то повертаємо контейнер з ним,
+        //Якщо запис існує, то повертаємо контейнер з записом,
         //якщо ні, то повертаємо порожній контейнер
         return Optional.ofNullable(subscription);
     }
 
-    //Пропишемо тіло методу для отримання списку користувачів банку
+    //Тіло методу для отримання списку користувачів банку
     @Override
     public List<User> getAllUsers() {
-        //Список користувачів перетворюється на потік для обробки, і з цього потоку
-        //оброблені дані збираються у список знов
+        //Список користувачів карток (без повторень) перетворюється на потік для обробки, і з цього потоку оброблені дані збираються у список знов
         return cards.values().stream().map(BankCard::getUser).distinct().collect(Collectors.toUnmodifiableList());
     }
 }
