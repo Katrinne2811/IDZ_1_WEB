d50f91ea3d20bd07518228ed20c33c293430cf3c
Katrinne2811 <160325742+Katrinne2811@users.noreply.github.com>
2025-03-24 13:44:42 +0100

    Update Subscription.java

--- jmp-dto/src/main/java/edu/nure/mjt/Subscription.java
+++ jmp-dto/src/main/java/edu/nure/mjt/Subscription.java
@@ -2,18 +2,19 @@ package edu.nure.mjt;
 
 import java.time.LocalDateTime;
 
+//Оголошення класу для створення запису про створення банк. картки
 public class Subscription
 {
-    private String bankcard;//приватна текстова змінна з номером картки
-    private LocalDateTime startDate; //приватна змінна типу дати з датою відкриття запису
+    private String bankcard;//Номер стовреної карти
+    private LocalDateTime startDate; //Дата відкриття картки
 
-    //Конструктор - як створити об'экт класу Subscription (опис даних картки з номером bankcard)
+    //Конструктор для створення запису про оформлення кбанк. картки
     public Subscription(String bankcard, LocalDateTime startDate) {
         this.bankcard = bankcard;
         this.startDate = startDate;
     }
 
-    //Геттери - методи, що дозволяють дізнатися значення змінних номеру картки чи дати відкриття ,банк. картки з даними Subscription
+    //Методи, що дозволяють дізнатися номер картки чи дату відкриття
     public String getBankcard() {
         return bankcard;
     }
@@ -21,8 +22,7 @@ public class Subscription
         return startDate;
     }
 
-    //Сеттери - методи, що дозволяють змінити значення змінних номеру картки чи дати відкриття картки з описом Subscription
-    //на вказані у дужках
+    //Методи, що дозволяють змінити номер картки чи дату відкриття
     public void setBankcard(String bankcard) { this.bankcard = bankcard; }
     public void setStartDate(LocalDateTime startDate) { this.startDate = startDate; }
 }
