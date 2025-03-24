9de231d94288c7a39027e4ea551a8a9f43cc1576
Katrinne2811 <160325742+Katrinne2811@users.noreply.github.com>
2025-03-24 13:33:24 +0100

    Update Bank.java

--- jmp-bank-api/src/main/java/edu/nure/mjt/api/Bank.java
+++ jmp-bank-api/src/main/java/edu/nure/mjt/api/Bank.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.time.Period;//Імпорт класу для обчислення часового інтервалу між 2 датами
 import java.util.function.Predicate;//Імпорт типу, що дозволяє перевіряти умову
 
-//"Правило", яке вказує, що треба створити банківську картку типу bankCardType користувача user
+//"Правило", яке вказує, що треба створити банківську картку типу bankCardType користувачу user
 public interface Bank {
     BankCard createBankCard(User user, BankCardType bankCardType);
 
