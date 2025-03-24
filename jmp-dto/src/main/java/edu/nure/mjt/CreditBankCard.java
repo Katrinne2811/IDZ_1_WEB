e831d73ea53d630e52cca889791780bf6a10690b
Katrinne2811 <160325742+Katrinne2811@users.noreply.github.com>
2025-03-24 13:37:34 +0100

    Update CreditBankCard.java

--- jmp-dto/src/main/java/edu/nure/mjt/CreditBankCard.java
+++ jmp-dto/src/main/java/edu/nure/mjt/CreditBankCard.java
@@ -2,21 +2,19 @@ package edu.nure.mjt;
 
 public class CreditBankCard extends BankCard {
 
-    private double creditLimit; // приватна числова змінна з крапкою -  сума ліміту кредитної картки
+    private double creditLimit; //Сума ліміту кредитної картки
 
     //Конструктор "картки" - вказує, як створити об'єкт класу  CreditBankCard - кредитну картку
     public CreditBankCard(String number, User user, double creditLimit) {
-        super(number, user, BankCardType.CREDIT); // Викликаємо конструктор батьківського класу BankCard, від якого
-        //дочірній клас CreditBankCard успадковує номер та користувача.
-        //До батьківського класу BankCard передається тип картки Credit
-        this.creditLimit = creditLimit; // додаємо нову змінну, якої нема в батьківської картки:
-        // "this.creditLimit" — змінній ліміту крединої картки в класі CreditBankCard присвоюється creditLimit з конструктора
+        super(number, user, BankCardType.CREDIT); // Викликаємо конструктор батьківського класу BankCard, від якого 
+        //дочірній клас CreditBankCard успадковує номер та користувача. До батьківського класу BankCard передається тип картки Credit
+        this.creditLimit = creditLimit; // додаємо змінну типу картки, якої нема в батьківському класі
     }
 
-    //Геттер - метод, який дозволяє дізнатися кредитний ліміт картки
+    //Метод, який дозволяє дізнатися кредитний ліміт картки
     public double getCreditLimit() { return creditLimit;}
 
-    //Сеттер - метод, що дозволяє змінити кредитний ліміт картки CreditBankCard на creditLimit з дужок
+    //Метод, що дозволяє змінити кредитний ліміт картки
     public void setCreditLimit(double creditLimit) {
         this.creditLimit = creditLimit;
     }
